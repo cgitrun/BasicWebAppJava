c020bfa8f190ffdd03d837c0c85c05c9a08f635a
cgitrun <44413762+cgitrun@users.noreply.github.com>
2024-12-03 20:13:21 +0000

    Enhancement: Created function to return true if input is a prime num. From geeksforgeeks.org

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -1,7 +1,19 @@
 package com.develogical;
 
 public class QueryProcessor {
+    public boolean isPrime(int n)
+    {
+        // Corner case
+        if (n <= 1)
+            return false;
 
+        // Check from 2 to n/2
+        for (int i = 2; i <= n / 2; i++)
+            if (n % i == 0)
+                return false;
+
+        return true;
+    }
     public String process(String query) {
 
         if (query.toLowerCase().contains("shakespeare")) {
