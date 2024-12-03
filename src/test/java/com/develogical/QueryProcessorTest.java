556de50c45b9c799332f231428ed470a6043673b
cgitrun <44413762+cgitrun@users.noreply.github.com>
2024-12-03 20:16:57 +0000

    Created tests to confirm query behavior

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -24,4 +24,24 @@ public class QueryProcessorTest {
     public void knowsTeamName() throws Exception {
         assertThat(queryProcessor.process("What is your name?"), containsString("RobTest"));
     }
+
+    @Test
+    public void findsPrimeNums() throws Exception {
+        assertThat(queryProcessor.process("Which of the following numbers are primes: 67, 29, 53, 10, 18"), containsString("67 29 53"));
+    }
+
+    @Test
+    public void findDifference() throws Exception {
+        assertThat(queryProcessor.process("What is 80 minus 37"), containsString("43"));
+    }
+
+    @Test
+    public void findSum() throws Exception {
+        assertThat(queryProcessor.process("What is 89 plus 39 plus 19"), containsString("147"));
+    }
+
+    @Test
+    public void findExponential() throws Exception {
+        assertThat(queryProcessor.process("What is 12 to the power of 2"), containsString("144"));
+    }
 }
